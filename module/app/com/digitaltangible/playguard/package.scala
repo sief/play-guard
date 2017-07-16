8e8bb776bfa7c192051d205cd4d1da22fb4518a8
Simon Effing <simon.effing@gmail.com>
2017-07-16 12:39:09 +0200

    fixed typo in header

--- module/app/com/digitaltangible/playguard/package.scala
+++ module/app/com/digitaltangible/playguard/package.scala
@@ -11,10 +11,10 @@ package object playguard {
       ip <- request.headers.get(configuredHeader)
     } yield ip) getOrElse {
 
-      // Consider X-Forward-For as most accurate if it exists
+      // Consider X-Forwarded-For as most accurate if it exists
       // Since it is easy to forge an X-Forwarded-For, only consider the last ip added by our proxy as the most accurate
       // https://en.wikipedia.org/wiki/X-Forwarded-For
-      request.headers.get("X-Forward-For").map(_.split(",").last.trim).getOrElse {
+      request.headers.get("X-Forwarded-For").map(_.split(",").last.trim).getOrElse {
         request.remoteAddress
       }
 
