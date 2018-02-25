4b5cb3812aff151f001370f82e903a6443414e83
Simon Effing <simon.effing@gmail.com>
2018-02-25 13:10:15 +0100

    XForwardedTrustImmediateConnectionRequestHandler test

---
+++ module/test/com/digitaltangible/playguard/XForwardedTrustImmediateConnectionRequestHandlerSpec.scala
@@ -0,0 +1,62 @@
+package com.digitaltangible.playguard
+
+import org.scalatest.MustMatchers
+import org.scalatestplus.play.PlaySpec
+import play.api.http.{DefaultHttpErrorHandler, HttpConfiguration}
+import play.api.mvc.Headers
+import play.api.mvc.request.RemoteConnection
+import play.api.routing.Router
+import play.api.test.FakeRequest
+
+class XForwardedTrustImmediateConnectionRequestHandlerSpec extends PlaySpec with MustMatchers {
+
+  val handler = new XForwardedTrustImmediateConnectionRequestHandler(
+    Router.empty,
+    DefaultHttpErrorHandler,
+    HttpConfiguration(),
+    Nil
+  )
+
+  "XForwardedTrustImmediateConnectionRequestHandler" should {
+    "get remote connection from header" in {
+      val req = FakeRequest().withHeaders(Headers("X-Forwarded-For" -> "1.1.1.1", "X-Forwarded-Proto" -> "https"))
+      handler.handlerForRequest(req)._1.remoteAddress mustBe "1.1.1.1"
+      handler.handlerForRequest(req)._1.secure mustBe true
+    }
+
+    "get remote connection from last entry in header" in {
+      val req =
+        FakeRequest().withHeaders(Headers("X-Forwarded-For" -> "1.1.1.1,2.2.2.2", "X-Forwarded-Proto" -> "http,https"))
+      handler.handlerForRequest(req)._1.remoteAddress mustBe "2.2.2.2"
+      handler.handlerForRequest(req)._1.secure mustBe true
+    }
+
+    "get remote connection from last entry in header in quotes" in {
+      val req = FakeRequest().withHeaders(
+        Headers("X-Forwarded-For" -> """"1.1.1.1","2.2.2.2"""", "X-Forwarded-Proto" -> """"http","https"""")
+      )
+      handler.handlerForRequest(req)._1.remoteAddress mustBe "2.2.2.2"
+      handler.handlerForRequest(req)._1.secure mustBe true
+    }
+
+    "get remote connection from multiple headers in different cases" in {
+      val req = FakeRequest().withHeaders(
+        Headers(
+          "X-Forwarded-For" -> "1.1.1.1",
+          "x-forwarded-for" -> "2.2.2.2",
+          "x-forwarded-proto" -> "http",
+          "X-Forwarded-Proto" -> "https"
+        )
+      )
+      handler.handlerForRequest(req)._1.remoteAddress mustBe "2.2.2.2"
+      handler.handlerForRequest(req)._1.secure mustBe true
+    }
+
+    "keep remote connection if no forwarded header in request" in {
+      val req =
+        FakeRequest().withConnection(RemoteConnection("1.1.1.1", false, None))
+      handler.handlerForRequest(req)._1.remoteAddress mustBe "1.1.1.1"
+      handler.handlerForRequest(req)._1.secure mustBe false
+    }
+  }
+}
